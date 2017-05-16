88f963bc0c8c70c0eaac80abc0e200427f659916
Mike Smith <grimbough@gmail.com>
2017-05-16 09:24:08 +0200

    Actually added the pkgconfig function!

---
+++ R/zzz.R
@@ -0,0 +1,29 @@
+pkgconfig <- function(opt = c("PKG_LIBS", "PKG_CPPFLAGS")) {
+    path <- system.file("lib", package="Rhdf5lib", mustWork=TRUE)
+    if (nzchar(.Platform$r_arch)) {
+        arch <- sprintf("/%s", .Platform$r_arch)
+    } else {
+        arch <- ""
+    }
+    patharch <- paste0(path, arch)
+    
+    result <- switch(match.arg(opt), 
+                     PKG_CPPFLAGS = {
+                         sprintf('-I"%s"', system.file("include", package="Rhdf5lib"))
+                     }, 
+                     PKG_LIBS = {
+                         switch(Sys.info()['sysname'], 
+                                Linux={
+                                    sprintf('-L%s -lhdf5_cpp -lhdf5 -ldl -lz',
+                                            patharch)
+                                }#, Darwin={
+                                #    sprintf('%s/libhts.a -lz -pthread', patharch)
+                                #}, Windows={
+                                #    sprintf('-L"%s" -lhts -lz -pthread -lws2_32', patharch)
+                                #}
+                         )
+                     }
+    )
+    
+    cat(result)
+}
