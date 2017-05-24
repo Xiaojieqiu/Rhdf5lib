a3c73e9f7be86cfec07544abfe280ba689b60f66
Mike Smith <grimbough@gmail.com>
2017-05-24 18:12:50 +0200

    Started work on a vignette for submission to Bioc

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,16 @@
-pkgconfig <- function(opt = c("PKG_CXX_LIBS", "PKG_C_LIBS","PKG_CPPFLAGS")) {
+#' Create layout plot of flowcell
+#' 
+#' This function returns values for \code{PKG_CXX_LIBS} and
+#' \code{PKG_CC_FLAGS} variables for use in Makevars files. 
+#' 
+#' @param opt A scalar character from the list of available options; 
+#' default is \code{PKG_CXX_LIBS}.
+#' @return \code{NULL}; prints the corresponding value to stdout.
+#' @examples
+#' pkgconfig("PKG_CXX_LIBS")
+#' pkgconfig("PKG_C_LIBS")
+#' @export
+pkgconfig <- function(opt = c("PKG_CXX_LIBS", "PKG_C_LIBS")) {
     path <- system.file("lib", package="Rhdf5lib", mustWork=TRUE)
     if (nzchar(.Platform$r_arch)) {
         arch <- sprintf("/%s", .Platform$r_arch)
@@ -8,9 +20,9 @@ pkgconfig <- function(opt = c("PKG_CXX_LIBS", "PKG_C_LIBS","PKG_CPPFLAGS")) {
     patharch <- paste0(path, arch)
     
     result <- switch(match.arg(opt), 
-                     PKG_CPPFLAGS = {
-                         sprintf('-I"%s"', system.file("include", package="Rhdf5lib"))
-                     }, 
+                    # PKG_CPPFLAGS = {
+                    #     sprintf('-I"%s"', system.file("include", package="Rhdf5lib"))
+                    # }, 
                      PKG_C_LIBS = {
                          switch(Sys.info()['sysname'], 
                                 Linux = {
